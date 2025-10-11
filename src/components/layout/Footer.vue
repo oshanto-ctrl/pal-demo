1a570ed2d8701dbb2843360875cc2e399f5a94b2
Oshanto-ctrl <siddikyrejoan@gmail.com>
2025-10-12 02:14:33 +0600

    Footer fixed from unwanted chars

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -44,31 +44,19 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
               </svg>
-<<<<<<< HEAD
               <span class="text-gray-400">Akash Plaza, Room #506 Plot #21, Road #7/D, Sector #09 Uttara, Dhaka, Bangladesh</span>
-=======
-              <span class="text-gray-400">123 Automation Street, Tech City, TC 12345</span>
->>>>>>> origin/master
             </div>
             <div class="flex items-center space-x-3">
               <svg class="w-5 h-5 text-prime-green" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
               </svg>
-<<<<<<< HEAD
               <a href="tel:+1234567890" class="text-gray-400 hover:text-prime-green transition-colors duration-300">+880 1769-093465</a>
-=======
-              <a href="tel:+1234567890" class="text-gray-400 hover:text-prime-green transition-colors duration-300">+1 (234) 567-890</a>
->>>>>>> origin/master
             </div>
             <div class="flex items-center space-x-3">
               <svg class="w-5 h-5 text-prime-green" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
               </svg>
-<<<<<<< HEAD
               <a href="mailto:info@primeautomations.com" class="text-gray-400 hover:text-prime-green transition-colors duration-300">salesdesk.primegroup@gmail.com</a>
-=======
-              <a href="mailto:info@primeautomations.com" class="text-gray-400 hover:text-prime-green transition-colors duration-300">info@primeautomations.com</a>
->>>>>>> origin/master
             </div>
           </div>
         </div>
